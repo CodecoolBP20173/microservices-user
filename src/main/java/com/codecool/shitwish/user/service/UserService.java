e88cdfdb43c15863dcff4eba898001c42ed6c3ec
Horinka Peter <horinka.peter@gmail.com>
2018-08-28 15:32:53 +0200

    added some skeleton codes to userservice

--- src/main/java/com/codecool/shitwish/user/service/UserService.java
+++ src/main/java/com/codecool/shitwish/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.codecool.shitwish.user.service;
 
+import com.codecool.shitwish.user.model.UserModel;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,4 +11,14 @@ public class UserService {
     private void update(){}
 
     public void delete(){}
+
+    public UserModel getUserById(int id){
+        UserModel user = new UserModel();
+        return user;
+    }
+
+    public UserModel getUserByEmail(String email){
+        UserModel user = new UserModel();
+        return user;
+    }
 }
