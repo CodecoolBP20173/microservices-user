b13dca8a87529655b4a016c0f2853db99fc0d9c6
Andrea Németh <nemandreanemeth@gmail.com>
2018-08-30 11:16:13 +0200

    sample user added again

--- src/main/java/com/codecool/shitwish/user/controller/UserController.java
+++ src/main/java/com/codecool/shitwish/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.codecool.shitwish.user.controller;
 
 import com.codecool.shitwish.user.model.UserModel;
 import com.codecool.shitwish.user.service.UserService;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,22 +12,22 @@ public class UserController {
     @Autowired
     private UserService service;
 
-//    @GetMapping("/sampleuser")
-//    public JSONObject sampleUser(){
-//        JSONObject obj = new JSONObject();
-//        obj.put("usrID", 2);
-//        obj.put("firstName", "Béláim");
-//        obj.put("lastName", "kecske");
-//        obj.put("email", "valami@akarmi.com");
-//        obj.put("phoneNumber", "12345678");
-//        obj.put("country", "france");
-//        obj.put("city", "paris");
-//        obj.put("zipCode", "2084");
-//        obj.put("street", "kossuth lajos utca");
-//        obj.put("profilePicture", "https://robohash.org/valami@akarmi.com.png");
-//
-//        return obj;
-//    }
+    @GetMapping("/sampleuser")
+    public JSONObject sampleUser(){
+        JSONObject obj = new JSONObject();
+        obj.put("usrID", 2);
+        obj.put("firstName", "Béláim");
+        obj.put("lastName", "kecske");
+        obj.put("email", "valami@akarmi.com");
+        obj.put("phoneNumber", "12345678");
+        obj.put("country", "france");
+        obj.put("city", "paris");
+        obj.put("zipCode", "2084");
+        obj.put("street", "kossuth lajos utca");
+        obj.put("profilePicture", "https://robohash.org/valami@akarmi.com.png");
+
+        return obj;
+    }
 
     @GetMapping("/user")
     public UserModel getUser(@RequestBody String email){
